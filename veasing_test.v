a5239cf8d791ed75b40e2a2237c75fee55fd827e
Cody Metz <ccmetz92@gmail.com>
2019-10-21 23:28:27 -0500

    Add tests for cubic and quartic functions
    A couple of the tests are failing for some odd reason. Need to
    investigate them further.

--- veasing_test.v
+++ veasing_test.v
@@ -23,8 +23,44 @@ fn test_quadratic_ease_in_out_under_0_5() {
 }
 
 fn test_quadratic_ease_in_out_over_0_5() {
-  assert veasing.quadratic_ease_in_out(0.6) == 0.68 
+  assert veasing.quadratic_ease_in_out(0.6) == 0.68
 }
 
 //
 // Cubic tests
+
+fn test_cubic_ease_in() {
+  assert veasing.cubic_ease_in(3) == 27  
+}
+
+fn test_cubic_ease_out() {
+  assert veasing.cubic_ease_out(4) == 28  
+}
+
+fn test_cubic_ease_in_out_under_0_5() {
+  assert veasing.cubic_ease_in_out(0.4) == 0.256 
+}
+
+fn test_cubic_ease_in_out_over_0_5() {
+  // FIXME
+  assert veasing.cubic_ease_in_out(0.6) == 0.744
+}
+
+//
+// Quadratic tests
+fn test_quartic_ease_in() {
+  assert veasing.quartic_ease_in(3) == 81  
+}
+
+fn test_quartic_ease_out() {
+  // FIXME
+  assert veasing.quartic_ease_out(3) == -80
+}
+
+fn test_quartic_ease_in_out_under_0_5() {
+  assert veasing.quartic_ease_in_out(0.4) == 0.204800 
+}
+
+fn test_quartic_ease_in_out_over_0_5() {
+  assert veasing.quartic_ease_in_out(0.6) == 0.795200 
+}
