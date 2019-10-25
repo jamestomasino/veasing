50af006461fef0d89cc8d24e02f17a43b4320776
Cody Metz <ccmetz92@gmail.com>
2019-10-24 19:45:09 -0500

    Incorporate helper method for comparing floats, add sine/circular tests

--- veasing_test.v
+++ veasing_test.v
@@ -1,88 +1,134 @@
 import veasing
 
+import math
+
 //
 // Linear tests
 
 fn test_linear_interpolation() {
-  assert veasing.linear_interpolation(2) == 2 
+  assert compare(veasing.linear_interpolation(2), 2) 
 }
 
 //
 // Quadratic tests
 
 fn test_quadratic_ease_in() {
-  assert veasing.quadratic_ease_in(2) == 4  
+  assert compare(veasing.quadratic_ease_in(2), 4)  
 }
 
 fn test_quadratic_ease_out() {
-  assert veasing.quadratic_ease_out(5) == -15 
+  assert compare(veasing.quadratic_ease_out(5), -15) 
 }
 
 fn test_quadratic_ease_in_out_under_0_5() {
-  assert veasing.quadratic_ease_in_out(0.4) == 0.32   
+  assert compare(veasing.quadratic_ease_in_out(0.4), 0.32)   
 }
 
 fn test_quadratic_ease_in_out_over_0_5() {
-  assert veasing.quadratic_ease_in_out(0.6) == 0.68
+  assert compare(veasing.quadratic_ease_in_out(0.6),  0.68)
 }
 
 //
 // Cubic tests
 
 fn test_cubic_ease_in() {
-  assert veasing.cubic_ease_in(3) == 27  
+  assert compare(veasing.cubic_ease_in(3),  27)  
 }
 
 fn test_cubic_ease_out() {
-  assert veasing.cubic_ease_out(4) == 28  
+  assert compare(veasing.cubic_ease_out(4), 28)  
 }
 
 fn test_cubic_ease_in_out_under_0_5() {
-  assert veasing.cubic_ease_in_out(0.4) == 0.256 
+  assert compare(veasing.cubic_ease_in_out(0.4),  0.256) 
 }
 
 fn test_cubic_ease_in_out_over_0_5() {
-  // FIXME
-  assert veasing.cubic_ease_in_out(0.6) == 0.744
+  assert compare(veasing.cubic_ease_in_out(0.6),  0.744)
 }
 
 //
 // Quadratic tests
 
 fn test_quartic_ease_in() {
-  assert veasing.quartic_ease_in(3) == 81  
+  assert compare(veasing.quartic_ease_in(3), 81)  
 }
 
 fn test_quartic_ease_out() {
-  // FIXME
-  assert veasing.quartic_ease_out(3) == -80
+  assert compare(veasing.quartic_ease_out(3), -15)
 }
 
 fn test_quartic_ease_in_out_under_0_5() {
-  assert veasing.quartic_ease_in_out(0.4) == 0.2048 
+  assert compare(veasing.quartic_ease_in_out(0.4), 0.2048) 
 }
 
 fn test_quartic_ease_in_out_over_0_5() {
-  assert veasing.quartic_ease_in_out(0.6) == 0.7952 
+  assert compare(veasing.quartic_ease_in_out(0.6), 0.7952) 
 }
 
 //
 // Quintic tests
 
 fn test_quintic_ease_in() {
-  assert veasing.quintic_ease_in(4) == 1024  
+  assert compare(veasing.quintic_ease_in(4), 1024)  
 }
 
 fn test_quintic_ease_out() {
-  assert veasing.quintic_ease_out(4) == 244  
+  assert compare(veasing.quintic_ease_out(4), 244)  
 }
 
 fn test_quintic_ease_in_out_under_0_5() {
-  assert veasing.quintic_ease_in_out(0.4) == 0.16384  
+  assert compare(veasing.quintic_ease_in_out(0.4), 0.16384)  
 }
 
 fn test_quintic_ease_in_out_over_0_5() { 
+  assert compare(veasing.quintic_ease_in_out(0.6), 0.83616)
+}
+
+//
+// Sine tests
+
+fn test_sine_ease_in() {
+  assert compare(veasing.sine_ease_in(3), 1.00)    
+}
+
+fn test_sine_ease_out() {
   // FIXME
-  assert veasing.quintic_ease_in_out(0.6) == 0.83616
+  assert compare(veasing.sine_ease_out(3), 0.000000) 
+}
+
+fn test_sine_ease_in_out() {
+  assert compare(veasing.sine_ease_in_out(3), 1.00) 
+}
+
+//
+// Circular tests
+
+fn test_circular_ease_in() {
+  assert compare(veasing.circular_ease_in(0.4), 0.083485) 
+}
+
+fn test_circular_ease_out() {
+  assert compare(veasing.circular_ease_out(0.4), 0.80) 
+}
+
+fn test_circular_ease_in_out_under_0_5() {
+  assert compare(veasing.circular_ease_in_out(0.4), 0.20) 
+}
+
+fn test_circular_ease_in_out_over_0_5() {
+  assert compare(veasing.circular_ease_in_out(0.6), 0.80) 
+}
+
+
+// Helper method for comparing floats
+fn compare(x f64, y f64) bool {
+  tolerance := 0.00001
+  diff := math.abs(x - y)
+  mean := math.abs(x + y) / 2.0
+  if math.is_nan(diff / mean) {
+    return true
+  }
+  return (diff / mean) < tolerance
 }
 
